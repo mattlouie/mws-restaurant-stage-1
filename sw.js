726753f64158c26fe5a59bc316706d8e11aa3f86
Matthew Louie <mlouie915@gmail.com>
2018-08-29 20:32:54 -0700

    Added ServiceWorker

---
+++ sw.js
@@ -0,0 +1,31 @@
+var filesToCache = [
+  '/',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
+var staticCacheName = 'pages-cache-v1.0';
+
+self.addEventListener('install', function(event) {
+  console.log('Installing service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
