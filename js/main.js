92190a6414c29913de2384d4461fc34365a6be86
Matthew Louie <mlouie915@gmail.com>
2018-08-31 21:28:33 -0700

    Added ServiceWorker to main.js

--- js/main.js
+++ js/main.js
@@ -209,3 +209,13 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration succeded:', registration);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+}else {
+  console.log('Service workers are not supported by this browser.');
+}
